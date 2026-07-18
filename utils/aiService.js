885f3323b0b312e98850083667916c49688b9af5
rakeshneopane <rakeshneopane@gmail.com>
2026-07-18 21:16:55 +0530

    feat: Add Headers x-api key

--- utils/aiService.js
+++ utils/aiService.js
@@ -3,16 +3,22 @@ const axios = require("axios");
 const AI_SERVICE_URL = process.env.AI_SERVICE_URL || "http://localhost:8000";
 
 const triggerCaptioning = async (imageId, imageUrl) => {
-  await axios.post(`${AI_SERVICE_URL}/caption`, {
-    image_id: imageId,
-    image_url: imageUrl,
+  await axios.post(`${AI_SERVICE_URL}/caption`, 
+    { image_id: imageId,image_url: imageUrl},
+    {
+    headers: {
+      "X-API-Key": process.env.INTERNAL_API_KEY,
+    }
   });
 };
 
 const searchImages = async (query, limit = 10) => {
-  const response = await axios.post(`${AI_SERVICE_URL}/search`, {
-    query,
-    limit,
+  const response = await axios.post(`${AI_SERVICE_URL}/search`, 
+    { query, limit},
+    {
+    headers: {
+      "X-API-Key": process.env.INTERNAL_API_KEY,
+    }
   });
   return response.data; // array of { image_id, caption, tags }
 };
